cfd1fc31a479cca511bb43cb93ea956b2304eec1
Daniel Dawson <randomsilo@gmail.com>
2021-04-18 18:14:23 -0500

    vue3 pulling from an api, but not mine, yet.

---
+++ simple-listing/userinterfaces/simple-listing-vue/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-app>
+    <v-main>
+      <div id="app">
+        <Sentences />
+      </div>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import Sentences from './components/Sentences.vue';
+
+export default {
+  name: 'App',
+
+  components: {
+    Sentences,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
